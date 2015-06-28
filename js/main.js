4e03ebf9841521de093968791dd8df17010707e9
Romello Goodman <romellogood@gmail.com>
2015-06-28 00:56:04 -0700

    Continuing Dev.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var playlist = {};
+    // accessToken = null;
+
 function search(type, query) {
   var searchUrl = 'https://api.spotify.com/v1/search?q=' + query + '&type=' + type;
   $.ajax({
@@ -6,7 +9,7 @@ function search(type, query) {
     url: searchUrl,
     crossDomain : true,
     xhrFields: {
-        withCredentials: false
+      withCredentials: false
     }
   })
   .done(function(data) {
@@ -17,10 +20,7 @@ function search(type, query) {
       appendToPage(data.tracks.items, 'track');
     }
   })
-  .fail( function(xhr, textStatus, errorThrown) {
-    alert(xhr.responseText);
-    alert(textStatus);
-  });
+  .fail( function(xhr, textStatus, errorThrown) { alert(xhr.responseText); });
 }
 
 //Clear previous data & append new data
@@ -57,40 +57,127 @@ function getArtistTracks(artistURL) {
     url: searchUrl,
     crossDomain : true,
     xhrFields: {
-        withCredentials: false
+      withCredentials: false
     }
   })
   .done(function(data) {
     // $('#search-query').text(artistName + '\'s Top Songs');
     appendToPage(data.tracks, 'track');
   })
-  .fail( function(xhr, textStatus, errorThrown) {
-    alert(xhr.responseText);
-    alert(textStatus);
-  });
+  .fail( function(xhr, textStatus, errorThrown) { alert(xhr.responseText); });
 }
 
+//Add into dictionary. Use dictionary so there won't be duplicates.
 function addToPlaylist(id) {
-  console.log(id);
-}
-
-function checkUser() {
-  var regex   = new RegExp("[\\?&]code=([^&#]*)"),
-      results = regex.exec(location.search);
-  if(results) {
-    return true;
-  } else {
-    return false;
+  if(playlist[id] === undefined) {
+    playlist[id] = 1;
+    /*jshint multistr: true */
+    $('#playlist-list').append(
+      '<li id="' + id +'" class="song"> \
+        <span id="' + id +'" class="remove" onclick="removeFromPlaylist(this.id)">X</span> \
+      One - One</li>'
+    );
   }
 }
 
+function removeFromPlaylist(id) {
+  $('#' + id).remove();
+  delete playlist[id];
+}
+
+function savePlaylist() {
+  var playlistData = { "uris": [] };
+  for(var song in playlist) {
+    playlistData.uris.push("spotify:track:" + song);
+  }
+  console.log(playlistData);
+  //Create the Playlist
+  $.ajax({
+    type: 'POST',
+    contentType: 'application/json',
+    dataType: 'jsonp',
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    },
+    url: 'https://api.spotify.com/v1/users/mellogood/playlists',
+    data: {
+      name: 'hello'
+    },
+    crossDomain : true,
+    xhrFields: {
+      withCredentials: false
+    }
+  })
+  .done(function(data) {
+    console.log('Created Playlist');
+    console.log(data);
+    //Add in the songs
+    // $.ajax({
+    //   type: 'POST',
+    //   dataType: 'application/json',
+    //   //TODO
+    //   url: 'https://api.spotify.com/v1/users/mellogood/playlists/data.uri/tracks',
+    //   data: playlistData,
+    //   crossDomain : true,
+    //   xhrFields: {
+    //     withCredentials: false
+    //   }
+    // })
+    // .done(function(data) {
+    //   console.log('Success!');
+    // })
+    // .fail( function(xhr, textStatus, errorThrown) { alert(xhr.responseText); });
+  })
+  .fail( function(xhr, textStatus, errorThrown) { alert(xhr.responseText); });
+}
+
+function getUserID() {
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: 'https://api.spotify.com/v1/me',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    },
+    crossDomain : true,
+    xhrFields: {
+      withCredentials: false
+    }
+  })
+  .done(function(data) {
+    console.log(data.id);
+  })
+  .fail( function(xhr, textStatus, errorThrown) { alert(xhr.responseText); });
+}
+
+//Check if they are logged in.
+function checkUser() {
+  accessToken = getHashValue('access_token');
+  if(accessToken) {
+    console.log(accessToken);
+    getUserID();
+  } else {
+    console.log('no token');
+  }
+}
+
+/* Spotify returns the access token in th url.
+ * This solution was taken from:
+ * http://stackoverflow.com/questions/11920697/how-to-get-hash-value-in-a-url-in-js
+ */
+function getHashValue(key) {
+  var matches = location.hash.match(new RegExp(key+'=([^&]*)'));
+  return matches ? matches[1] : null;
+}
+
 $(document).ready(function () {
-  var playlist = {};
-  console.log(checkUser());
+  checkUser();
+  // console.log(getUserID());
   search('artist', 'chance');
+  //Differentiate between saving and searching
   $('input').keyup(function (e) {
-    if (e.keyCode == 13 && $('input').val() !== '') {
-      search($('select').val(),$('input').val());
+    if (e.keyCode == 13 && $('#search-input').val() !== '') {
+      search($('select').val(),$('#search-input').val());
     }
   });
 });
