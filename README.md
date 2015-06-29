817d7ee669267346e44a3c9c734d6fa45226ab11
Romello Goodman <romellogood@gmail.com>
2015-06-29 10:29:03 -0700

    Edited request process, gitignore and readme. Deleted build folder. Various small changes.

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ run `grunt` to concat and minify everything.
 
 * jQuery
 * Bootstrap
+
+##Webapp Flow
+1. Check if the user is logged in & getAccessToken
+  * If not: ask to login
+2. Get the user's userID
+3. User search by artist or track
+  * By artist
+    1. Get the artist's uri from spotify and get their top tracks
+  * By Track
+    1. Return the results from Spotify's search
+4. User clicks on a track
+  * Add that track the playlist and store in a dictionary
+5. User adds multiple tracks
+6. User names & creates the playlist
+7. User has created spotify playlist
--- js/main.js
+++ js/main.js
@@ -2,8 +2,29 @@ var playlist    = {},
     accessToken = '',
     userID      = '';
 
+function makeRequest(type, url, params, cbk) {
+  reqBody = {
+    'type': type,
+    'url': url,
+    'crossDomain': true,
+    'xhrFields': {
+      'withCredentials': false
+    }
+  };
+  for(var key in params) {
+    reqBody[key] = params[key];
+  }
+  $.ajax(reqBody)
+    .done(cbk)
+    .fail(function(xhr, textStatus, errorThrown) {
+      alert(xhr.responseText);
+    });
+}
+
 function search(type, query) {
-  if(query !== '') {
+  if(query === '') {
+    warn('Sorry, that was an empty search.');
+  } else {
     makeRequest(
       'get',
       'https://api.spotify.com/v1/search?q=' + query + '&type=' + type,
@@ -18,8 +39,6 @@ function search(type, query) {
         }
       }
     );
-  } else {
-    warn('Sorry there weren\'t any search parameters.');
   }
 }
 
@@ -31,31 +50,30 @@ function warn(message) {
 //Clear previous data & append new data
 function appendToPage(data, type) {
   $('#search-results').html(' ');
-  if(data.length !== 0) {
+  if(data.length === 0) {
+    warn('Sorry, we couldn\'t find that.');
+  } else {
     for(var result in data) {
       if(type == 'artist') {
         /*jshint multistr: true */
         $('#search-results').append(
-          '<div class="result"> \
-            <p id="' + data[result].id + '" onclick="getArtistsTracks(this.id)">' + data[result].name +'</p> \
-          </div>'
-      );
+          '<p class="query-result" id="' + data[result].id +
+          '" onclick="getArtistsTracks(this.id)">' + data[result].name +'</p>'
+        );
       } else {
-        //ID = artistName-songName-songID
+        /* ID = artistName{-]songName{-]songID
+         * Using {-] because it is a unique string and won't appear in a song title
+         */
         $('#search-results').append(
-          '<div class="query-result"> \
-            <p> \
-              <span class="glyphicon glyphicon-plus add-button" id="' +
-              data[result].artists[0].name + '{-]' + data[result].name + '{-]' + data[result].id +
-              '" onclick="addToPlaylist(this.id)"></span>' +
-              data[result].artists[0].name + '- ' + data[result].name +
-            '</p> \
-          </div>'
+          '<p class="query-result" id="' + data[result].artists[0].name + '{-]' +
+          data[result].name + '{-]' + data[result].id +
+          '" onclick="addToPlaylist(this.id)"> \
+            <span class="glyphicon glyphicon-plus add-button"></span>' +
+            data[result].artists[0].name + '- ' + data[result].name +
+          '</p>'
         );
       }
     }
-  } else {
-    warn('Sorry we couldn\'t find that.');
   }
 }
 
@@ -72,7 +90,7 @@ function getArtistsTracks(artistID) {
   );
 }
 
-//ID Form = artistName-songName-songID
+//ID Form = artistName{-]songName{-]songID
 function addToPlaylist(id) {
   id = id.split('{-]');
   //Add songs into dictionary. Use dictionary so there won't be duplicates.
@@ -80,25 +98,33 @@ function addToPlaylist(id) {
     playlist[id[2]] = 1;
     /*jshint multistr: true */
     $('#playlist-list').append(
-      '<p id="' + id[2] +'" class="song"> \
-        <span class="glyphicon glyphicon-remove remove-button" id="' + id[2] +'" \
-        onclick="removeFromPlaylist(this.id)"></span> \
-      ' + id[0] + '- ' + id[1] +'</p>'
+      '<p class="playlist-song" id="' + id[2] +
+      '" onclick="removeFromPlaylist(this.id)"> \
+        <span class="glyphicon glyphicon-remove remove-button"></span>' +
+        id[0] + '- ' + id[1] +
+      '</p>'
     );
   }
 }
 
+//Delete from #playlist-list and the dictionary
 function removeFromPlaylist(id) {
   $('#' + id).remove();
   delete playlist[id];
 }
 
 function savePlaylist() {
-  if(accessToken !== null) {
-    if($('#playlist-input').val() !== "") {
-      var playlistData = { "uris": [] };
+  if(accessToken === null) {
+    warn('Please login to save your playlist.');
+  } else {
+    if($('#playlist-input').val() === "") {
+      warn('Please name your playlist first.');
+    }
+    else {
+      var playlistData = { 'uris': [] };
+      //Move the songs from the dictionary to the array.
       for(var song in playlist) {
-        playlistData.uris.push("spotify:track:" + song);
+        playlistData.uris.push('spotify:track:' + song);
       }
       console.log($('#playlist-input').val());
       console.log(playlistData);
@@ -139,17 +165,13 @@ function savePlaylist() {
           );
         }
       );
-    } else {
-      warn('Please name your playlist first.');
     }
-  } else {
-    warn('Please Log-In to save the playlist.');
   }
 }
 
 function logout() {
   //TODO - change from localhost
-  $('#login-status').html('<a href="https://accounts.spotify.com/authorize?client_id=66dbe5ac0dc04f3ca53b78f802c07153&redirect_uri=http://localhost:8000&scope=playlist-modify%20playlist-modify-public%20&response_type=token&state=123"><p>Log-In Again</p>');
+  $('#login-status').html('<a href="https://accounts.spotify.com/authorize?client_id=66dbe5ac0dc04f3ca53b78f802c07153&redirect_uri=http://localhost:8000&scope=playlist-modify%20playlist-modify-public%20&response_type=token&state=123"><p>LogIn</p>');
 }
 
 function getUserID() {
@@ -172,46 +194,24 @@ function getUserID() {
   );
 }
 
-//Check if they are logged in.
-function checkUser() {
-  accessToken = getHashValue('access_token');
-  if(accessToken) {
-    getUserID();
-  }
-}
 
 /* Spotify returns the access token in th url.
  * This solution was taken from:
  * http://stackoverflow.com/questions/11920697/how-to-get-hash-value-in-a-url-in-js
  */
-function getHashValue(key) {
-  var matches = location.hash.match(new RegExp(key+'=([^&]*)'));
-  return matches ? matches[1] : null;
-}
-
-function makeRequest(type, url, params, cbk) {
-  reqBody = {
-    'type': type,
-    'url': url,
-    crossDomain : true,
-    xhrFields: {
-      withCredentials: false
-    }
-  };
-  for(var key in params) {
-    reqBody[key] = params[key];
+function checkLoginStatus() {
+  matches = location.hash.match(new RegExp('access_token=([^&]*)'));
+  accessToken = matches ? matches[1] : null;
+  if(accessToken) {
+    getUserID();
+  } else {
+    warn('First, login to Spotify!');
   }
-  $.ajax(reqBody)
-    .done(cbk)
-    .fail(function(xhr, textStatus, errorThrown) {
-      alert(xhr.responseText);
-    });
 }
 
 $(document).ready(function () {
-  checkUser();
-  //TODO - remove
-  // search('artist', 'childish gambino');
+  checkLoginStatus();
+  // search('artist', 'childish gambino'); //TODO - remove
   //Excute different operations based on which input is focused
   $('input').keyup(function (e) {
     if(e.keyCode == 13) {
